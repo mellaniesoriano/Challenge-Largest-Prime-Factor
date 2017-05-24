7f13aa37ceabc310abdbb93f101530efe1bc18a3
mellaniesoriano <msoriano808@gmail.com>
2017-05-24 07:47:49 -1000

    tests pass

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,13 @@
-exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
+exports.largestPrimeFactor = function(n) {
+  var primeNumber = 2; // first prime number
   // do your work here
 
+  while (n > 1) { // as long as n > 1 stays true, the code block will keep looping
+    if (n % primeNumber === 0) { // if n is evenly divided by primeNumber
+      n /= primeNumber; // divide n by primeNumber
+    } else {
+      primeNumber++; // else increase primeNumber by 1 & loop again until n is no longer greater than 1 (n = 1).
+    }
+  }
   return primeNumber;
 };
